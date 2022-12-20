e811a4f8ab6456a1a017c9dba9a939765a16f38b
Tode Temov <75773817+temov@users.noreply.github.com>
2022-12-20 02:59:02 +0100

    Add files via upload

---
+++ Bonus Homework/script.js
@@ -0,0 +1,22 @@
+// Print the nymbers of given array and their Sum
+
+
+
+let recipeNameElement = document.getElementById('recipe');
+let recipeName = recipeNameElement.value;
+let recipeHeading = document.getElementById('recipeTitle');
+
+let math = document.getElementById('mathEq');
+
+function printRecipe(){
+
+
+  
+    recipeHeading.innerText = recipeName;
+
+    // sumElement.innerHTML= `The sum of all numbers in the array is: ${sum} `;
+
+}
+printRecipe();
+
+
